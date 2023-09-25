851582a999e569d25c0e719eaa464c0817d4254b
harin <harin5959@kakao.com>
2023-09-25 13:07:12 +0900

    함수

--- 3_Function/main.cpp
+++ 3_Function/main.cpp
@@ -1,66 +1,161 @@
 #include <stdio.h>
 
-//함수형
+// 함수형
 // unsigned int : 반환 자료형<-Output
-// CaculatorSum : 함수 이름
-// (int x, int y) : 매개 변수(파라미터)<-Input
-unsigned int CaculatorSum(int x, int y)
+// CalculatorSum : 함수 이름
+// (int x, int y) : 매개 변수(파라메타)<-Input
+unsigned int CalculatorSum(int x, int y)
 {//->함수 시작
 	printf("함수 시작했다.\n");
 
-	return x+y;//<-반환 및 종료 처리
+	return x + y;//<-반환 및 종료 처리
 
 	printf("함수 끝났다.\n");
 }//->함수 끝
 
-//빼기 함수 만들기
-unsigned int CalculatorMinus(int x, int y)
+// 빼기 함수 만들기
+// CalculatorMinus
+int CalculatorMinus(int x, int y)
 {
-	int diff = 0;
-	diff = x - y;
-	return diff;
+	return x - y;
+}
+// 곱하기 함수 만들기
+// CalculatorMultiply
+int CalculatorMultiply(int x, int y)
+{
+	return x * y;
 }
 
-//곱하기 함수 만들기
-unsigned int CalculatorMultiply(int x, int y)
+// 나누기 함수 만들기
+// CalculatorDivide
+float CalculatorDivide(float x, float y)
 {
-	int product = 0;
-	product = x * y;
-	return product;
+	return x / y;
 }
 
-//나누기 함수 만들기
-unsigned int CalculatorDevide(int x, int y)
+// 나머지 함수 만들기
+// CalculatorRemain
+float CalculatorRemain(int x, int y)
 {
-	int division = 0;
-	division = x / y;
-	return division;
+	return x % y;
 }
 
-//나머지 함수 만들기
-unsigned int CalculatorRemain(int x, int y)
+// Factorial 함수
+int Factorial(int Num)
 {
-	int remain = 0;
-	remain = x % y;
-	return remain;
+	int FacResult = 1;
+	for (int i = Num; i > 0; --i)
+	{
+		FacResult *= i;
+	}
+
+	return FacResult;
+}
+
+int Factorial2(int Num)
+{
+	int FacResult = 1;
+
+	for (int i = 1; i <= Num; ++i)
+	{
+		FacResult *= i;
+	}
+
+	return FacResult;
+}
+
+int ReFactorial(int Num)
+{
+	if (Num == 1)
+	{
+		return 1;
+	}
+
+	return Num * ReFactorial(Num - 1);
+}
+
+// SumToN 함수
+int SumToN(int Num)
+{
+	int FacResult = 0;
+	for (int i = Num; i > 0; --i)
+	{
+		FacResult += i;
+	}
+
+	return FacResult;
+}
+
+void CallFunction();
+
+int main()
+{
+	//unsigned int nResult = 0;
+	//nResult = CalculatorSum(1, 1);
+	//printf("더하기 함수 실행 결과 = %d\n", nResult);
+	//nResult = CalculatorMinus(1, 1);
+	//printf("빼기 함수 실행 결과 = %d\n", nResult);
+	//nResult = CalculatorMultiply(2, 2);
+	//printf("곱하기 함수 실행 결과 = %d\n", nResult);
+	//float fResult = 0.0f;
+	//fResult = CalculatorDivide(10, 3);
+	//printf("나누기 함수 실행 결과 = %f\n", fResult);
+	//fResult = CalculatorRemain(10, 4);
+	//printf("나머지 함수 실행 결과 = %f\n", fResult);
+	//CallFunction();
+
+	//// 반복문
+	//// for문
+	//for (/*초기 설정*/int i = 0;/*반복 조건 채크*/i < 10;/*반복 후 처리*/++i)
+	//{// <-반복 처리할 코드 시작
+	//	
+	//	printf("%d Hello\n", i);
+	//}// <-반복 처리할 코드 끝
+
+	//int a = 0;
+	//for (; a < 5; ++a)
+	//{
+	//	printf("HelloA\n");
+	//}
+
+	//for (int b = 0;b < 10;b+=2)
+	//{
+	//	printf("HelloB\n");
+	//}
+	
+	// 1. int i = 0;
+	// 2. i = 0이고, i < 10 채크 결과는 true(1)
+	// 3. 반복 처리할 코드 실행
+	// 4. 반복 후 처리 ++i(i를 1증가 시킨다.) i = 1;
+	//~~~~~~~~~~~~~
+	// 1. int i = 9;
+	// 2. i = 9이고, i < 10 채크 결과는 true(1)
+	// 3. 반복 처리할 코드 실행
+	// 4. 반복 후 처리 ++i(i를 1증가 시킨다.) i = 10;
+	// 1. int i = 10;
+	// 2. i = 10이고, i < 10 채크 결과는 false(0)
+
+
+	//for (int i = 0;i < 10;++i)
+	//{
+	//	printf("Hello\n");
+	//}
+
+
+	// Factorial
+	// 5 Factorial 구해라
+	// 5 * 4 * 3 * 2 * 1 = 15; 
+
+	printf("Factorial FacResult = %d\n", Factorial(5));
+	printf("Factorial2 FacResult = %d\n", Factorial2(5));
+	printf("ReFactorial FacResult = %d\n", ReFactorial(5));
+	printf("SumToN Result = %d\n", SumToN(10));
+
+	//1~n까지 합 구하는 함수
+	//int SumToN(int Num)
 }
 
 void CallFunction()
 {
-
-	printf("함수 호출됐다 !\n");
-}
-
-int main()
-{
-	unsigned int nResult = 0;
-	CallFunction();
-	nResult = CaculatorSum(1, 1);
-	printf("덧셈 함수 실행 결과 = %d\n", nResult);
-	nResult = CalculatorMinus(75, 30);
-	printf("뺄셈 함수 실행 결과 = % d\n", nResult);
-	nResult = CalculatorDevide(75, 30);
-	printf("나눗셈 함수 실행 결과 = %d\n", nResult);
-	nResult = CalculatorRemain(75, 30);
-	printf("나머지 함수 실행 결과 = %d\n", nResult);
+	printf("함수 호출됬다!");
 }
